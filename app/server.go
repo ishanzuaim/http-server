c4ba6ee5dcc09d06da85f1fbac4873ff726e6690
Ishan Zuaim <ishan.zuaim@oneteam.services>
2024-04-27 15:51:05 +0930

    added file logic.

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -20,13 +21,14 @@ func main() {
 		throw_error("Failed to bind to port 4221")
 	}
 
+	args := os.Args[1:]
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			throw_error("Error accepting connection: ", err.Error())
 		}
 		fmt.Println("\nIncoming connection request from ", conn.RemoteAddr().String())
-		go handleConnection(conn)
+		go handleConnection(conn, args)
 	}
 }
 
@@ -45,57 +47,89 @@ func parseHeader(header string) map[string]string {
 	return header_map
 }
 
+func getHeader(key, value string) string {
+	return fmt.Sprintf("%s: %s\r\n", key, value)
+}
 func appendBody(sendData string, body string) string {
-	sendData += "Content-Type: text/plain\r\n"
-	sendData += fmt.Sprintf("Content-Length: %d\r\n", len(body))
+	sendData += getHeader("Content-Type", "text/plain")
+	sendData += getHeader("Content-Length", strconv.Itoa(len(body)))
 	sendData += "\r\n"
 	sendData += body
 	return sendData
 }
 
-func sendData(conn net.Conn, status int, body string) {
-	sendData := fmt.Sprintf("HTTP/1.1 %d ", status)
+func getStatusLine(status int) string {
+	statusLine := fmt.Sprintf("HTTP/1.1 %d ", status)
 	switch status {
 	case 200:
-		sendData += "OK"
+		statusLine += "OK"
 	case 404:
-		sendData += "Not Found"
-	default:
-		os.Exit(1)
+		statusLine += "Not Found"
 	}
-	sendData += "\r\n"
-	if body != "" {
-		sendData = appendBody(sendData, body)
-	} else {
-		sendData += "\r\n"
-	}
-	fmt.Printf("Sending: %s\n", sendData)
-	conn.Write([]byte(sendData))
+	statusLine += "\r\n"
+	return statusLine
 }
 
-func echoData(url string) string {
-	_, after, _ := strings.Cut(url, "/echo/")
+func sendData(conn net.Conn, status int, body string) {
+	data := getStatusLine(status)
+	if body != "" {
+		data = appendBody(data, body)
+	} else {
+		data += "\r\n"
+	}
+	fmt.Printf("Sending: %s\n", data)
+	conn.Write([]byte(data))
+}
+
+func sendFile(conn net.Conn, file, directory string) {
+	data := getStatusLine(200)
+	data += getHeader("Content-Type", "application/octet-stream")
+
+	absolutePath := fmt.Sprintf("%s/%s", directory, file)
+	fmt.Println(absolutePath)
+	fileData, err := os.ReadFile(absolutePath)
+	if err != nil {
+		fmt.Println("Could not find the file")
+		sendData(conn, 404, "")
+		return
+	}
+
+	fmt.Println(fileData)
+	data += getHeader("Content-Length", strconv.Itoa(len(fileData)))
+	data += "\r\n"
+	data += string(fileData)
+	conn.Write([]byte(data))
+}
+
+func getSuffix(url, initial string) string {
+	_, after, _ := strings.Cut(url, initial)
 	return after
 }
 
-func handleURL(conn net.Conn, headerMap map[string]string) {
+func handleURL(conn net.Conn, headerMap map[string]string, args []string) {
 	url := headerMap["url"]
 	if url == "/" {
 		sendData(conn, 200, "")
 	} else if strings.HasPrefix(url, "/echo") {
-		sendData(conn, 200, echoData(url))
+		sendData(conn, 200, getSuffix(url, "/echo/"))
 	} else if strings.HasPrefix(url, "/user-agent") {
 		sendData(conn, 200, headerMap["agent"])
+	} else if strings.HasPrefix(url, "/files") {
+		filePath := getSuffix(url, "/files/")
+		if len(args) > 1 && args[0] == "--" {
+			sendFile(conn, filePath, args[1])
+		} else {
+			sendData(conn, 404, "")
+		}
 	} else {
 		sendData(conn, 404, "")
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, args []string) {
 	connectionBytes := make([]byte, 1024)
 	conn.Read(connectionBytes)
 	headerMap := parseHeader(string(connectionBytes))
 	fmt.Printf("Recieved %s for %s\n\n", headerMap["method"], headerMap["url"])
-	handleURL(conn, headerMap)
-
+	handleURL(conn, headerMap, args)
 }
